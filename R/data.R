6626d0242b52cfa61806c1affb303391bdfd5dda
Mehmet Gencer <mehmetgencer@yahoo.com>
2023-11-12 18:35:51 +0300

    Added Türkiye migration data

---
+++ R/data.R
@@ -0,0 +1,36 @@
+#' Türkiye migration network flows data
+#'
+#' This data frame contains the data on migration of people between Türkiye's provinces
+#' in the period 2016-2017-2018.
+#' This is a consolidated version of raw data from Turkish Statistical Institute.
+#'
+#' Each row contains the number of people migrated in the "weight" column. 'from' and 'to'
+#' columns include the codes of provinces as used in the Türkiye statistical coding system.
+#'
+#' The familiar names of provinces and their locations are to be found in a separate data frame
+#' named TurkiyeMigration.nodes
+#' @format ## `TurkiyeMigration.flows`
+#' A data frame with 6480 rows and 3 columns:
+#' \describe{
+#'   \item{from, to}{codes of origin and arrival province}
+#'   \item{weight}{number of people migrated}
+#' }
+#' @source <https://data.tuik.gov.tr/Kategori/GetKategori?p=Nufus-ve-Demografi-109>
+"TurkiyeMigration.flows"
+
+#' Türkiye migration network provinces/nodes data
+#'
+#' This data frame contains the data on Türkiye's provinces as used in the migration flows
+#' data frame (TurkiyeMigration.flow)
+#'
+#' Each row contains the code of the province as used in TurkiyeMigration.flows data frame,
+#' it is known name/label, and latitude/longitude of the province central.
+#'
+#' @format ## `TurkiyeMigration.nodes`
+#' A data frame with 6480 rows and 3 columns:
+#' \describe{
+#'   \item{id}{codes of province as used in the TurkiyeMigration.flows data frame}
+#'   \item{label}{Name of the province capital city}
+#'   \item{longitude, latitude}{spatial coordinates of the province capital}
+#' }
+"TurkiyeMigration.nodes"
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Hello World
+# rSDI: Spatial Dispersion Index (SDI) for R
+
+This package provides functions for calculating Spatial Dispersion Index ([Gençer, 2023](https://dx.doi.org/10.1007/s12061-023-09545-8)) for R, and its visualization.
+
+Spatial Dispersion Index (SDI) is a family of indices to measure spatial range of relations in a spatial/geographic network. The index was developed as part of a large-scale socio-economic study on flows between cities, and has proven itself useful for evaluating spatial range of flows for the whole networ or its nodes.
+
+This package provides all variants of SDI as described in the paper (Gençer, 2023) and also provides functions to visualize index measurements on a network visualization plotted as a graph on a geographic map or a spatial base plate of your choosing.
