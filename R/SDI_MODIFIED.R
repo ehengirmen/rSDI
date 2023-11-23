49b6aa6b7fb2753d2db0135fc41da45579906792
mucahitzor <mucahit.zor@izmirekonomi.edu.tr>
2023-11-24 00:55:35 +0300

    if g has no dist attributes + adds edge distance attributes
    Then,
    + adds SDI output as vertex att.

--- R/SDI_MODIFIED.R
+++ R/SDI_MODIFIED.R
@@ -1,28 +1,42 @@
-# distance.calculation kısmını SDI fonksiyonuna ekledim
+# base fun:
+# SDI(toyGraph, level="vertex", directionality="undirected", weight.use="weighted")
+
+# change variant arg to weight.use
+# directionality to be included. for now all considered as directed, thus: n**d**w, n**d**u etc.
 
 SDI_MODIFIED <- function (g, distance.calculation = NULL, level="vertex",variant="weighted",mode="all") {
   # added distance calculation
   if(!is.null(distance.calculation)){
     if(!distance.calculation %in% c('Haversine', 'Euclidean')) stop("Invalid 'distance.calculation' argument")
-    distances <- dist_calc(g, formula = distance.calculation)
-    E(g)$distance <- distances
+    g <- dist_calc(g, formula = distance.calculation)
   } else {
     # if null: check if distance is already calculated
     if(is.null(E(g)$distance)){
-      stop("No 'distance' attribute found in edges.
-           Please provide a 'distance.calculation' method or
-           ensure the graph has pre-calculated distances")
+      g <- dist_calc(g)
     }
   }
   ## rest is the same
   if (! mode %in% c("in","out","all")) stop("Invalid 'mode' argument")
+
   if (level=="network") {
-    if (variant=="weighted") weightedNetworkSDI(g)
-    else if (variant=="unweighted") unweightedNetworkSDI(g)
+    if (variant=="weighted") {
+      V(g)$ndw <- weightedNetworkSDI(g)
+      return(g)
+    }
+    else if (variant=="unweighted"){
+      V(g)$ndu <- unweightedNetworkSDI(g)
+      return(g)
+    }
     else stop("Invalid 'variant' argument")
   } else if (level=="vertex") {
-    if (variant=="weighted") weightedAllVerticesSDI(g, mode=mode)
-    else if (variant=="unweighted") unweightedAllVerticesSDI(g, mode=mode)
+    if (variant=="weighted") {
+      V(g)$vdw <- weightedAllVerticesSDI(g, mode=mode)
+      return(g)
+    }
+    else if (variant=="unweighted") {
+      V(g)$vdu <- unweightedAllVerticesSDI(g, mode=mode)
+      return(g)
+    }
     else stop("Invalid 'variant' argument")
   } else {
     stop("Invalid 'level' argument")
