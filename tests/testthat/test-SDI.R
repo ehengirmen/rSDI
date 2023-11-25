ba7ae3b3eb5de8f91b876a1c6c87eb696607820f
Mehmet Gencer <mehmetgencer@yahoo.com>
2023-11-25 09:54:33 +0300

    Vignette is extended with visualization usage examples

--- tests/testthat/test-SDI.R
+++ tests/testthat/test-SDI.R
@@ -6,3 +6,19 @@ test_that("SDI call with empty graph", {
   tg<-igraph::make_empty_graph(n = 0, directed = TRUE)
   expect_error(SDI(tg), "Call on an empty graph")
 })
+
+flows<-data.frame(from=c("A","B","A"), to=c("B","A","C"), weight=c(10,20,5))
+nodes<-data.frame(id=c("A","B","C","D"),x=c(0,4,0,4),y=c(3,0,0,3))
+flowsWithDistances<-flows
+flowsWithDistances$distance <- c(5,5,3)
+library(igraph)
+g<-graph_from_data_frame(flows, directed=TRUE, vertices=nodes)
+
+test_that("distance calc", {
+  # 'formula' is a special name in R, don't use
+  expect_equal(dist_calc(g, mode="Euclidean"), c(5,5,3))
+})
+
+test_that("SDI-vwa", {
+  expect_equal(V(SDI(g))$, c(5,5,3))
+})
